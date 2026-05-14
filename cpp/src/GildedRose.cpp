82ccca40548d8a89877c95cf9193fb75295faf86
seoulhan <seoulhanknox@gmail.com>
2026-05-14 17:15:38 +0900

    Refactoring

--- cpp/src/GildedRose.cpp
+++ cpp/src/GildedRose.cpp
@@ -1,56 +1,39 @@
 #include "GildedRose.h"
 
-GildedRose::GildedRose(std::vector<Item>& items) : items(items) {}
+const std::string AGED_BRIE = "Aged Brie";
+const std::string BACKSTAGE_PASSES =
+    "Backstage passes to a TAFKAL80ETC concert";
+const std::string SULFURAS = "Sulfuras, Hand of Ragnaros";
+
+GildedRose::GildedRose(std::vector<Item> &items) : items(items) {}
 
 void GildedRose::updateQuality() {
-    for (size_t i = 0; i < items.size(); i++) {
-        if (items[i].name != "Aged Brie"
-                && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (items[i].quality > 0) {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
-        } else {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1;
+  for (Item &item : items) {
+    if (item.name == SULFURAS) {
+    } else if (item.name == AGED_BRIE) {
+      item.quality++;
+      item.sellIn--;
+      if (item.sellIn < 0)
+        item.quality++;
+      item.quality = std::min(50, item.quality);
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (items[i].sellIn < 11) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
+    } else if (item.name == BACKSTAGE_PASSES) {
+      item.quality++;
+      if (item.sellIn < 11)
+        item.quality++;
+      if (item.sellIn < 6)
+        item.quality++;
+      item.sellIn--;
+      if (item.sellIn < 0)
+        item.quality = 0;
+      item.quality = std::min(50, item.quality);
 
-                    if (items[i].sellIn < 6) {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-
-        if (items[i].sellIn < 0) {
-            if (items[i].name != "Aged Brie") {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (items[i].quality > 0) {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                } else {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                }
-            }
-        }
+    } else {
+      item.quality--;
+      item.sellIn--;
+      if (item.sellIn < 0)
+        item.quality--;
+      item.quality = std::max(0, item.quality);
     }
+  }
 }
