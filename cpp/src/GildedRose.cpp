ddeae1eac3e6b1ef4b8636b7bad5613d98dcfb94
seoulhan <seoulhanknox@gmail.com>
2026-05-15 08:40:31 +0900

    파일목록 변경

--- cpp/src/GildedRose.cpp
+++ cpp/src/GildedRose.cpp
@@ -5,6 +5,48 @@ const std::string BACKSTAGE_PASSES =
     "Backstage passes to a TAFKAL80ETC concert";
 const std::string SULFURAS = "Sulfuras, Hand of Ragnaros";
 
+class AgedBrieUpdater {
+public:
+  void update(Item &item) {
+    item.quality++;
+    item.sellIn--;
+    if (item.sellIn < 0)
+      item.quality++;
+    item.quality = std::min(50, item.quality);
+  }
+};
+
+class BackstagePassUpdater {
+public:
+  void update(Item &item) {
+    item.quality++;
+    if (item.sellIn < 11)
+      item.quality++;
+    if (item.sellIn < 6)
+      item.quality++;
+    item.sellIn--;
+    if (item.sellIn < 0)
+      item.quality = 0;
+    item.quality = std::min(50, item.quality);
+  }
+};
+
+class SulfurasUpdater {
+public:
+  void update(Item &item) { return; }
+};
+
+class NormalItemUpdater {
+public:
+  void update(Item &item) {
+    item.quality--;
+    item.sellIn--;
+    if (item.sellIn < 0)
+      item.quality--;
+    item.quality = std::max(0, item.quality);
+  }
+};
+
 GildedRose::GildedRose(std::vector<Item> &items) : items(items) {}
 
 void GildedRose::updateQuality() {
@@ -12,11 +54,11 @@ void GildedRose::updateQuality() {
     if (item.name == SULFURAS) {
       continue;
     } else if (item.name == AGED_BRIE) {
-      updateAgedBrie(item);
+      AgedBrieUpdater().update(item);
     } else if (item.name == BACKSTAGE_PASSES) {
-      updateBackstagePasses(item);
+      BackstagePassUpdater().update(item);
     } else {
-      updateNormalItem(item);
+      NormalItemUpdater().update(item);
     }
   }
 }
